e428e0032435f23b39a45b9da234a9a92e55baf1
unknown <aks4u786@gmail.com>
2026-08-10 12:24:49 +0530

    refact: footer2

--- frontend/src/components/publiccomponents/layout/PublicFooter.tsx
+++ frontend/src/components/publiccomponents/layout/PublicFooter.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { LatticeBackground } from '../DesignSystem';
-import { Heart } from 'lucide-react';
 
 const PublicFooter: React.FC = () => {
   const scrollToTop = () => {
